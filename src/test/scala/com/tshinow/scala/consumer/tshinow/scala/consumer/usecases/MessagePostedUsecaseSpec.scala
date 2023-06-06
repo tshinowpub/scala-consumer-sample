aca31063eb871cf744381882e90c844d8bb7596c
tshinowpub <tshinow@gmail.com>
2023-06-07 04:40:06 +0900

    Fix usecase test.

--- src/test/scala/com/tshinow/scala/consumer/tshinow/scala/consumer/usecases/MessagePostedUsecaseSpec.scala
+++ src/test/scala/com/tshinow/scala/consumer/tshinow/scala/consumer/usecases/MessagePostedUsecaseSpec.scala
@@ -1,18 +1,24 @@
 package com.tshinow.scala.consumer.tshinow.scala.consumer.usecases
 
-import com.tshinow.scala.consumer.dbadapter.memory.OnMemoryMessageRepository
-import com.tshinow.scala.consumer.domain.channel.{ AccountId, ChannelId, MessageId }
+import com.tshinow.scala.consumer.domain.channel.{ AccountId, ChannelId, MessageId, MessageRepository, PostedMessage }
 import com.tshinow.scala.consumer.usecases.MessagePostedUsecase
+import org.scalamock.scalatest.AsyncMockFactory
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
 
-class MessagePostedUsecaseSpec extends AsyncWordSpec with Matchers {
+class MessagePostedUsecaseSpec extends AsyncWordSpec with Matchers with AsyncMockFactory {
 
   "MessagePostedUsecase" when {
     "aaaaが指定" should {
 
       "メッセージが投稿されたイベントから保存＆通知できる" in {
-        val usecase = new MessagePostedUsecase(new OnMemoryMessageRepository)
+        val repository = mock[MessageRepository]
+
+        val message = PostedMessage(MessageId("111"), ChannelId("222"), AccountId("2222"), "")
+
+        (repository.add _).expects(message).once()
+
+        val usecase = new MessagePostedUsecase(repository)
 
         val _ = usecase.run(MessageId("111"), ChannelId("222"), AccountId("2222"), "")
 
